3add11986cd298c76e85c2a690d302fd2c415b34
Aaron Guerrero <aaguerrero0901@gmail.com>
2025-02-18 10:24:33 -0800

    more updates

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
-import serial
 import numpy as np
 import matplotlib.pyplot as plt
-import serial.tools
 import serial.tools.list_ports
 
 ports = serial.tools.list_ports.comports()
@@ -15,18 +13,19 @@ for one in ports:
 com = input("Select Com Port for Arduino")
 
 for i in range(len(portsList)):
-    if portsList[i].startswith("com" + str(com)):
+    if portsList[i].startswith("COM" + str(com)):
         use = "COM" + str(com)
         print(use)
 
-
-
+serialInst = serial.Serial()
 serialInst.baudrate = 9600
 serialInst.port = use
 serialInst.open()
 
+data = np.random.random((12,12))
+
 while True: 
-    command = input("Arduino Command (ON/OFF/exit): ")
+    command = input("Arduino Command (ON/OFF/blink/exit): ")
     serialInst.write(command.encode('utf-8'))
      
     if command == 'exit':
